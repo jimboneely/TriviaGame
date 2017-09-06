bb9b522e15ce31d30dd6323e1b77a0e7d0012c69
James Neely <jimboneely@Jamess-MacBook-Pro.local>
2017-09-06 16:12:15 -0400

    Completed Homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,125 @@
+$(document).ready(function() {
+// Create a function that creates the start button and initial screen
+
+function initialScreen() {
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Let's Go!</a></p>";
+	$(".mainArea").html(startScreen);
+}
+
+initialScreen();
+
+//Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
+
+$("body").on("click", ".start-button", function(event){
+	event.preventDefault();  // added line to test issue on GitHub Viewer
+	clickSound.play();
+	generateHTML();
+
+	timerWrapper();
+
+}); // Closes start-button click
+
+$("body").on("click", ".answer", function(event){
+	//answeredQuestion = true;
+	clickSound.play();
+	selectedAnswer = $(this).text();
+	if(selectedAnswer === correctAnswers[questionCounter]) {
+		//alert("correct");
+
+		clearInterval(theClock);
+		generateWin();
+	}
+	else {
+		//alert("wrong answer!");
+		clearInterval(theClock);
+		generateLoss();
+	}
+}); // Close .answer click
+
+$("body").on("click", ".reset-button", function(event){
+	clickSound.play();
+	resetGame();
+}); // Closes reset-button click
+
+});  //  Closes jQuery wrapper
+
+function generateLossDueToTimeOut() {
+	unansweredTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 or other amount
+}
+
+function generateWin() {
+	correctTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 or other amount
+}
+
+function generateLoss() {
+	incorrectTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000); //  change to 4000 or other amount
+}
+
+function generateHTML() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function wait() {
+	if (questionCounter < 7) {
+	questionCounter++;
+	generateHTML();
+	counter = 30;
+	timerWrapper();
+	}
+	else {
+		finalScreen();
+	}
+}
+
+function timerWrapper() {
+	theClock = setInterval(thirtySeconds, 1000);
+	function thirtySeconds() {
+		if (counter === 0) {
+			clearInterval(theClock);
+			generateLossDueToTimeOut();
+		}
+		if (counter > 0) {
+			counter--;
+		}
+		$(".timer").html(counter);
+	}
+}
+
+function finalScreen() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function resetGame() {
+	questionCounter = 0;
+	correctTally = 0;
+	incorrectTally = 0;
+	unansweredTally = 0;
+	counter = 30;
+	generateHTML();
+	timerWrapper();
+}
+
+var startScreen;
+var gameHTML;
+var counter = 30;
+var questionArray = ["Who wrote “Game of Thrones”?", "How many legitimate children do Ned and Catelyn Stark have?", "What is Robert Baratheon's weapon of choice?", "Who goes by the knickname Little Finger?", "Why was Ser Jorah Mormont exiled from Westeros?", "Why is Daenerys called Khaleesi?", "Tyrion said My brother Jamie has his sword, I have my …", "What is the name of Ned Stark’s sword?"];
+var answerArray = [["George R.R. Martin", "George Orwell", "George Jefferson", "Georgie Boy"], ["1","5","6","3"], ["Valerian Steel Sword", "Dragon Glass", "Warhammer", "Mase"], ["Qybyn","The Queen","Petyr Baelish","Ned Stark"], ["Selling Dragon Glass", "Selling Valerian Steel", "Selling Magic", "Selling Slaves"], ["Dothraki for Mother","She is Azor Ahai","It Sounded Cool","She is a Khal"], ["Wine", "Mind", "Jokes", "Gold"], ["Money","Light Bringer","Needle","Ice"]];
+var correctAnswers = ["A. George R.R. Martin", "B. 5", "C. Warhammer", "C. Petyr Baelish", "D. Selling Slaves", "A. Dothraki for Mother", "B. Mind", "D. Ice"];
+var questionCounter = 0;
+var selecterAnswer;
+var theClock;
+var correctTally = 0;
+var incorrectTally = 0;
+var unansweredTally = 0;
+var clickSound = new Audio("sound/button-click.mp3");
